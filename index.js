56bd1f4f0119c465e2ee0386fabd3d5a89c327d3
M. Andrés Piana <mateoapiana@gmail.com>
2024-09-09 15:05:07 -0300

    Fix the cors

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 import router from "./routes/register.route.js"
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 export const app = express()
+
+const ACCEPTED_ORIGINS = [
+  'https://encuesta-ia-nn1g9b31d-mateo-amateurs-projects.vercel.app/'
+]
+
+const corsMiddleware = ({ acceptedOrigins = process.env.UI_URL ? process.env.UI_URL : ACCEPTED_ORIGINS } = {}) => cors({
+  origin: (origin, callback) => {
+    if (acceptedOrigins.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+})
+
+app.use(corsMiddleware())
 
 app.use(express.json())
 
